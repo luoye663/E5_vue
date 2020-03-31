2a0a7b3013c41407f16908254fb832842497d9fc
APLS <html6@msn.com>
2020-03-31 23:08:00 +0800

    备份

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,62 +1,64 @@
 <template>
-  <div>
-    <div style="margin: 0 auto;height: 10%;width: 95%;">
-      <h2>E5续订</h2>
+    <div>
+        <div style="margin: 0 auto;height: 10%;width: 95%;">
+            <h2>E5续订</h2>
+        </div>
+        <div style="margin: 0 auto;width: 95%;">
+            <Row>
+                <Col span="4">col-4</Col>
+                <Col span="16">
+                    <div>
+                        <Tabs>
+                            <TabPane label="基础配置" name="name1">
+                                <!--保存key-->
+                                <Card style="width:350px">
+                                    <p slot="title">Key配置</p>
+                                    <div>
+                                        <Input v-model="outlook.client_id" placeholder="client_id" class="input_value">
+                                            <span slot="prepend">client_id</span>
+                                        </Input>
+                                        <Input v-model="outlook.client_secret" placeholder="client_secret"
+                                               class="input_value">
+                                            <span slot="prepend">client_secret</span>
+                                        </Input>
+                                        <Button class="input_value">保存</Button>
+                                        <Button type="primary" class="input_value">授权</Button>
+                                    </div>
+                                </Card>
+                                <br/>
+                                <!--调用时间-->
+                                <Card style="width:350px">
+                                    <p slot="title">调用时间配置</p>
+                                    <div>
+                                        <Input v-model="cron_time.cron_time" placeholder="单位: 秒" class="input_value">
+                                            <span slot="prepend">调用时间间隔</span>
+                                        </Input>
+                                        <Input v-model="cron_time.cron_time_random_start"
+                                               placeholder="格式：10-60" class="input_value">
+                                            <span slot="prepend" class="input_value">随机时间范围</span>
+                                        </Input>
+                                        <Button type="primary" class="input_value">保存</Button>
+                                    </div>
+                                </Card>
+                                <br/>
+                                <!--操作-->
+                                <Card style="width:350px">
+                                    <p slot="title">操作</p>
+                                    <!--                  <Switch true-color="#13ce66" false-color="#ff4949"/>-->
+                                    <Button type="error" class="input_value">删除本网站账号及所有信息</Button>
+                                </Card>
+                            </TabPane>
+                            <!--日志显示-->
+                            <TabPane label="日志查询" name="name2">
+                                <Table :columns="log" :data="log_data"></Table>
+                            </TabPane>
+                        </Tabs>
+                    </div>
+                </Col>
+                <Col span="4">col-4</Col>
+            </Row>
+        </div>
     </div>
-    <div style="margin: 0 auto;width: 95%;">
-      <Row>
-        <Col span="4">col-4</Col>
-        <Col span="16">
-          <div>
-            <Tabs>
-              <TabPane label="基础配置" name="name1">
-                <!--保存key-->
-                <Card style="width:350px">
-                  <p slot="title">Key配置</p>
-                  <div>
-                    <Input v-model="value" placeholder="client_id" class="input_value">
-                      <span slot="prepend">client_id</span>
-                    </Input>
-                    <Input v-model="value" placeholder="client_secret" class="input_value">
-                      <span slot="prepend">client_secret</span>
-                    </Input>
-                    <Button class="input_value">保存</Button>
-                    <Button type="primary" class="input_value">授权</Button>
-                  </div>
-                </Card>
-                <br/>
-                <!--调用时间-->
-                <Card style="width:350px">
-                  <p slot="title">调用时间配置</p>
-                  <div>
-                    <Input v-model="value" placeholder="单位: 秒" class="input_value">
-                      <span slot="prepend">调用时间间隔</span>
-                    </Input>
-                    <Input v-model="value" placeholder="格式：10-60" class="input_value">
-                      <span slot="prepend" class="input_value">随机时间范围</span>
-                    </Input>
-                    <Button type="primary" class="input_value">保存</Button>
-                  </div>
-                </Card>
-                <br/>
-                <!--操作-->
-                <Card style="width:350px">
-                  <p slot="title">操作</p>
-                  <!--                  <Switch true-color="#13ce66" false-color="#ff4949"/>-->
-                    <Button type="error" class="input_value">删除本网站账号及所有信息</Button>
-                </Card>
-              </TabPane>
-              <!--日志显示-->
-              <TabPane label="日志查询" name="name2">
-                <Table :columns="log" :data="log_data"></Table>
-              </TabPane>
-            </Tabs>
-          </div>
-        </Col>
-        <Col span="4">col-4</Col>
-      </Row>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -66,6 +68,15 @@ export default {
     return {
       theme: 'ligth',
       value: 'test',
+      outlook: {
+        client_id: '',
+        client_secret: ''
+      },
+      cron_time: {
+        cron_time: 0,
+        cron_time_random_start: 0,
+        cron_time_random_end: 0
+      },
       log: [
         {
           title: '调用时间',
@@ -103,13 +114,13 @@ export default {
 </script>
 
 <style scoped>
-  .wrapper-header-nav-list {
-    height: inherit;
-    float: right;
-  }
+    .wrapper-header-nav-list {
+        height: inherit;
+        float: right;
+    }
 
-  .input_value {
-    width: 300px;
-    margin: 5px;
-  }
+    .input_value {
+        width: 300px;
+        margin: 5px;
+    }
 </style>
